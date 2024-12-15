68a850849d80348e9944ac2429dd92a83dc68b45
Bruna Camilly <brunagomes.ads@gmail.com>
2024-12-15 18:14:41 -0300

    implementação de telas

---
+++ app/src/main/java/com/rocketseat/nlw/nearby/ui/theme/component/welcome/WelcomeHeader.kt
@@ -0,0 +1,43 @@
+package com.rocketseat.nlw.nearby.ui.theme.component.welcome
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.rocketseat.nlw.nearby.R
+import com.rocketseat.nlw.nearby.ui.theme.Typography
+
+@Composable
+fun WelcomeHeader(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxSize()
+            .padding(horizontal = 40.dp, vertical = 48.dp)
+            .verticalScroll(state = rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.img_logo_logo_icon),
+            contentDescription = "Nearby App Logo"
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(text = "Boas vindas ao Nearby", style = Typography.headlineLarge)
+        Text(
+            text = "Tenha cupons de vantagens para usar em meus estabelecimentos favoritos",
+            style = Typography.bodyLarge,
+        )
+    }
+}
---
+++ app/src/main/java/com/rocketseat/nlw/nearby/ui/theme/screen/HomeScreen.kt
@@ -0,0 +1,16 @@
+package com.rocketseat.nlw.nearby.ui.theme.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize()) {
+
+    }
+
+
+}
